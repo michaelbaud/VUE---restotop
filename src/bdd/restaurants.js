569d30090d2f467fec7287f219e4422567b0db4d
michael baudoin <mika77290@live.fr>
2021-07-07 17:50:16 +0200

    competed bdd

--- src/bdd/restaurants.js
+++ src/bdd/restaurants.js
@@ -67,79 +67,79 @@ export default [
     },
     {
       name: 'O\'Plaza',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8wZWU5OTNkNy04NTZmLTQ0OTgtYTExNy0wZmI1MGQxYjBkNWMud2VicA==',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC82MGIxMGE1My1mYzU2LTRiNTYtOTA3My0xYjkxNTY0MjgyZWIuanBlZw==',
       note: '3.8',
       drive_time: '35 - 45mn'
     },
     {
-      name: 'Subway',
+      name: 'Burger King',
       image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==',
       note: '4.5',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'McDonald\'s' ,
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9iODQ1NDA4Zi1lOGFmLTRkMDUtYWI4YS0yNWMwYzVjMGI4YWUuanBlZw==',
+      name: 'Smash Burger' ,
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9kNjU2NTI5Mi1jZTBlLTQ0MTUtOGU5Yy05Y2M2NWY5NTcyMTguanBlZw==',
       note: '3.9',
       drive_time: '15 - 25mn'
     },
     {
-      name: 'O\'Tacos',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9mOGQ5MjU5ZS00YjIzLTRkNWYtOWQ1YS0wNjBlNjE2NDI4Y2QuanBlZw==',
+      name: 'Pitaya',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9mNWQwYjU1Ni1lZTlhLTRkMWMtYjRmYS1iYjhmMzc4MjM0ODYuanBlZw==',
       note: '4.4',
       drive_time: '15 - 25mn'
     },
     {
-      name: 'Pizza Hut',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zYWY1Y2IzMi0yZjYxLTQ5OWQtYjZjOC1iZWIzMDM0MmM4M2IuanBlZw==',
+      name: 'Paul',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC85M2Q2ZGU0Mi1hZjc0LTQzZDctYjI4Ny1hODA2OTUxMDFkOTguanBlZw==',
       note: '4.1',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'Momiji',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC80YWExZDYxOC0wZTQwLTRiZjItYjRjNS1hZTAyZDQ2Y2Y1Y2UuanBlZw==',
+      name: 'Maestro Pizza',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9hNmZmZDkxOC1jYzE3LTRlNDEtODE4ZC0wNDc2OGYyYjFlYzcud2VicA==',
       note: '4.3',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'Food Time',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9jNTMxMTVhOC0wZjExLTQ5YmUtYjIyNi0yYTc5YjgzMTA4MmI=',
+      name: 'Quick',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9lMDg2YTg0My1mMDQyLTRhZTctYmYzYS01MjhiY2Y0YTAzZTMuanBlZw==',
       note: '4.2',
       drive_time: '25 - 35mn'
     },
     {
-      name: 'L\'escale',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC81OTI5MzU4Ni01NDk4LTQ5YTYtOGMzZS1lMzYzNTBkYmMzMTMuanBlZw==',
+      name: 'Bigdal',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8wYzZmYjEzMy0zNmI5LTQxNTAtOTA0ZS1jNjhiYzk5ODkwM2MuanBlZw==',
       note: '4.5',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'M&A',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kdXl0NGg5bmZuajUwLmNsb3VkZnJvbnQubmV0L3Jlc2l6ZWQvMTU0NDA4NzMyMjk3Ni13NTUwLTdiLmpwZw==',
+      name: 'Point B',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9mZWEyMTEzYy1lNDA3LTQ2OWMtYjIyNi0yYzQyOGIzZWViMjMuanBlZw==',
       note: '4.3',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'Burger\'s Friens',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC81Mzk3YWEzOC0yYjRhLTRmOTItYmU4Ny1kZDE5YWEzNGU4MTQuanBlZw==',
+      name: 'Big M',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9lMzJiYTAyNS0yOGI0LTRkZWYtOTZhOC03MDVmNjcwNjQ3YTcuanBlZw==',
       note: '3.8',
       drive_time: '30 - 40mn'
     },
     {
-      name: 'Les Pyramides',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC9mNWI2NDI3Zi1lOWNkLTRmNjAtOGI3Ni00YWUyZWNjMmM5ODQuanBlZw==',
+      name: 'G La Dal',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8xYzkxMjk5MC00MTIwLTQ2OGMtYTlkOC0zYmQyNTRlNzBiNTEuanBlZw==',
       note: '3.2',
       drive_time: '20 - 30mn'
     },
     {
-      name: 'Fête à crêpe',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC85YmUzOTI2My00YjIxLTRjNzItYTEzMy05ODlkOTJkOTM4NjIuanBlZw==',
+      name: 'Chicken Spot',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8yMDgzZmEzOS01NWM1LTRlYzktODIwNS0zY2E2M2MwMThiNDQuanBlZw==',
       note: '3.5',
       drive_time: '25 - 35mn'
     },
     {
-      name: 'O\'Plaza',
-      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8wZWU5OTNkNy04NTZmLTQ0OTgtYTExNy0wZmI1MGQxYjBkNWMud2VicA==',
+      name: 'O Time',
+      image: 'https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC85NTFmNzg2MC00ZjVkLTRiYTgtYTQ1MC00OGYyZGQ5YzA1OGQuanBlZw==',
       note: '3.8',
       drive_time: '35 - 45mn'
     },
